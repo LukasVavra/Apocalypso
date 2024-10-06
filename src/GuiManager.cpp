3d1831d3347f329e96a8195fb7e631a2908cc33b
Bc. Lukas Vavra <lucovavra@gmail.com>
2024-10-06 17:15:05 +0200

    imgui clean up fix

--- src/GuiManager.cpp
+++ src/GuiManager.cpp
@@ -17,6 +17,9 @@ bool GuiManager::init(SDL_Window *window, SDL_Renderer *renderer)
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); 
     (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Conrols
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGui::StyleColorsLight();
     ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer2_Init(renderer);
     return true;
@@ -29,10 +32,11 @@ void GuiManager::handle_event(SDL_Event *event)
 
 void GuiManager::update()
 {
+    bool close_btn = true;
     ImGui_ImplSDLRenderer2_NewFrame();
     ImGui_ImplSDL2_NewFrame();
     ImGui::NewFrame();
-    ImGui::Begin("Hello, Dear ImGui with SDL2");
+    ImGui::Begin("Hello, Dear ImGui with SDL2", &close_btn);
     ImGui::Text("This is just a basic Hello World!");
     ImGui::End();
     ImGui::Render();
@@ -45,6 +49,7 @@ void GuiManager::render()
 
 void GuiManager::clean()
 {
+    ImGui_ImplSDLRenderer2_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
 }
